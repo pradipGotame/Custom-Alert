9391941ab3cf2ab87eef5dcb83a6df5dd620ae2d
Pradip Gotame <pradip.gotame@hitchedapp.com>
2019-10-24 15:25:48 +0545

    Initial Commit

---
+++ CustomAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomAlert
+//
+//  Created by Pradip Gotame on 10/24/19.
+//  Copyright © 2019 Pradip Gotame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
