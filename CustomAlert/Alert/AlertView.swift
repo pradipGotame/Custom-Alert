61e173cba4c29ddfffaf139b4f4797d26fc98960
Pradip Gotame <pradip.gotame@hitchedapp.com>
2019-10-24 17:06:08 +0545

    first commit

---
+++ CustomAlert/Alert/AlertView.swift
@@ -0,0 +1,21 @@
+//
+//  AlertView.swift
+//  CustomAlert
+//
+//  Created by Pradip Gotame on 10/24/19.
+//  Copyright © 2019 Pradip Gotame. All rights reserved.
+//
+
+import UIKit
+
+class AlertView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
